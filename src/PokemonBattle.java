6479ab1fad87cc18e357b88bab6ffeaf3c8c34f6
batsoarr000 <batsoarr000@CAM-LAB-09.bgsd.k12.wa.us>
2019-11-01 07:49:18 -0700

    1/3 done

--- src/PokemonBattle.java
+++ src/PokemonBattle.java
@@ -1,9 +1,27 @@
-
+//import code to enable user interface,create scanner in scope of entire project, create three seperate methods, add three methods to main, allow  battlestart(); method to have the user input the name of the pokemon, 
+import java.util.*;
 public class PokemonBattle {
-
+	static Scanner userInput = new Scanner(System.in);
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String Pokemon = battlestart();
+		damage();
+		statsTable();
 
+	}
+	public static String battlestart() {
+		System.out.println("Trainer Brock has appeared!!!");
+		System.out.println("Zebstrika appeared!!!");
+		System.out.print("Which Pokemon do you choose? ");
+		String name = userInput.nextLine();
+		System.out.println("You chose " + name + "!!!");
+		System.out.println("It’s a Pokemon battle between Arcanine and Zebstrika! Go!");
+		return name;
+		}
+	public static void damage() {
+		
+	}
+	public static void statsTable() {
+		
 	}
 
 }
